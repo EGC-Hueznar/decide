07f20752e6a931806726e72b6ea53f2eef3f9cdd
dml1994 <dml94@live.com>
2020-12-28 14:18:25 +0100

    VI-14 - Añadido test de creacion del bot de telegram #116
    Con este test se comprueba a traves del parametro username que el bot creado es el correcto.

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,23 @@
+import telegram
 from django.test import TestCase
+from base.tests import BaseTestCase
 
 # Create your tests here.
+class VisualizerTestCase(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+
+    def create_bot(self):    
+        bot_token = '1415070510:AAE49OJPu4viYNo5Tfov4vzkoIyeNf_JBr4'
+        bot = telegram.Bot(bot_token)
+        return bot
+
+    def test_create_bot(self):
+        bot = self.create_bot()
+        id = "decidehueznarbot"
+        self.assertEqual(bot.username,id)
+
