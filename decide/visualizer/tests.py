4f47ba29b0cee2ea8b9687e42c36da08f80ce77a
dml1994 <dml94@live.com>
2020-12-30 16:13:09 +0100

    VI015 - Creacion del Test de envio de mensajes de texto
    - Elaborado un test para comprobar el funcionamiento del Bot de Telegram a la hora de enviar mensajes de texto.
    - Añadida la dependencia de la API de Telegram en el archivo "requirements.txt" del proyecto.

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -11,13 +11,21 @@ class VisualizerTestCase(BaseTestCase):
     def tearDown(self):
         super().tearDown()
 
+    # Método para crear el objeto bot
     def create_bot(self):    
         bot_token = '1415070510:AAE49OJPu4viYNo5Tfov4vzkoIyeNf_JBr4'
         bot = telegram.Bot(bot_token)
         return bot
 
-    def test_create_bot(self):
+    # Test de creación y obtención de los datos del bot de telegram
+    def test_create_bot_telegram(self):
         bot = self.create_bot()
         id = "decidehueznarbot"
         self.assertEqual(bot.username,id)
 
+    # Test de envío de mensaje de texto al grupo de Telegram
+    def test_send_message_telegram(self):
+        bot = self.create_bot()
+        id = -1001318632551
+        message = bot.send_message(id,"Test de envío de mensaje de texto")
+        self.assertEqual(message.chat.title,"Actualizaciones Decide Huéznar")
