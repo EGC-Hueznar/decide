64802f661ff440f4b7bde719a2940961a7f69369
dml1994 <dml94@live.com>
2021-01-04 01:09:12 +0100

    VI018 - Añadido un metodo para enviar un reporte a Telegram #135
    Modificado el views.py para añadir el metodo que envia el reporte a Telegram de una votacion tipo "voting"
    Modificado el urls.py para añadir la direccion del metodo.
    Añadido un boton en el template para llamar a la funcionalidad.

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import VisualizerView
+from .views import *
+
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
+    path('<int:voting_id>/<telegram>', send_telegram_report),
 ]
