df8c21a58910b28d61c0171baa42c9c7b5f53ff8
fraromgon2us <fraromgon2@alum.us.es>
2021-01-04 18:13:40 +0100

    Arreglo/VOVI-001: Solucionada la incidencia
    Se ha añadido el import del modulo necesario

--- decide/voting/admin.py
+++ decide/voting/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils import timezone
 
 from .models import *
-
+#from decide.visualizer.telegrambot import *
 from .filters import StartedFilter
 
 #Votaciones Binarias
@@ -75,47 +75,48 @@ class PreguntaMultipleAdmin(admin.ModelAdmin):
 class OpcionMultipleAdmin(admin.ModelAdmin):
     list_display = ('id','nombre_opcion','n_votado','Nombre_Pregunta_Multiple')
 
+#send_telegram_report(v)
 
-def start(modeladmin, request, queryset):
-    for v in queryset.all():
-        v.create_pubkey()
-        v.start_date = timezone.now()
-        v.save()
+# def start(modeladmin, request, queryset):
+#     for v in queryset.all():
+#         v.create_pubkey()
+#         v.start_date = timezone.now()
+#         v.save()
+#
+#
+# def stop(ModelAdmin, request, queryset):
+#     for v in queryset.all():
+#         v.end_date = timezone.now()
+#         v.save()
+#
+#
+# def tally(ModelAdmin, request, queryset):
+#     for v in queryset.filter(end_date__lt=timezone.now()):
+#         token = request.session.get('auth-token', '')
+#         v.tally_votes(token)
+#
+#
+# class QuestionOptionInline(admin.TabularInline):
+#     model = QuestionOption
+#
+#
+# class QuestionAdmin(admin.ModelAdmin):
+#     inlines = [QuestionOptionInline]
+#
+#
+# class VotingAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'start_date', 'end_date')
+#     readonly_fields = ('start_date', 'end_date', 'pub_key',
+#                        'tally', 'postproc')
+#     date_hierarchy = 'start_date'
+#     list_filter = (StartedFilter,)
+#     search_fields = ('name', )
+#
+#     actions = [ start, stop, tally ]
 
 
-def stop(ModelAdmin, request, queryset):
-    for v in queryset.all():
-        v.end_date = timezone.now()
-        v.save()
-
-
-def tally(ModelAdmin, request, queryset):
-    for v in queryset.filter(end_date__lt=timezone.now()):
-        token = request.session.get('auth-token', '')
-        v.tally_votes(token)
-
-
-class QuestionOptionInline(admin.TabularInline):
-    model = QuestionOption
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [QuestionOptionInline]
-
-
-class VotingAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_date', 'end_date')
-    readonly_fields = ('start_date', 'end_date', 'pub_key',
-                       'tally', 'postproc')
-    date_hierarchy = 'start_date'
-    list_filter = (StartedFilter,)
-    search_fields = ('name', )
-
-    actions = [ start, stop, tally ]
-
-
-admin.site.register(Voting, VotingAdmin)
-admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Voting, VotingAdmin)
+# admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(VotacionBinaria,VotacionBinariaAdmin)
 admin.site.register(RespuestaBinaria,RepuestaBinariaAdmin)
