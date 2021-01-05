8865bc7d2641e59074f8f92ce2c298a7e2c6452b
marcadsan2 <marcadsan2@alum.us.esgit config --global user.email marcadsan2@alum.us.es>
2021-01-06 00:21:23 +0100

    VI-020: Envio resultado de una votacion a Twitter

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
     path('<int:voting_id>/telegram_report', telegram_report),
+    path('<int:voting_id>/twitter_report', twitter_report),
 ]
