62f403f473b949c2c518a98a8cbf2351e3af816f
manuelagt <manuelagt@icloud.com>
2021-01-05 23:54:08 +0100

    feat(Admin) created Admin layout

--- decide/booth/decide-react/src/components/Admin.js
+++ decide/booth/decide-react/src/components/Admin.js
@@ -1,16 +1,85 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 
 export default class Census extends Component {
 
- render() {
+  render() {
 
-        return (
-        <View>
-            <Text>Bienvenido Admin!</Text>
-            <Text>Ver todas las votaciones creadas</Text>
-            <Text>Ver todos los usuarios</Text>
+     return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.title}>¡Bienvenido Admin!</Text>
+                <Text>Desde este portal puedes gestionar tanto las votaciones creadas como los usuarios registrados.</Text>
+                <View style={styles.sections}>
+                    <View style={styles.button}>
+                        <Button color="linear-gradient(top, #049cdb, #0064cd)" title="Votaciones"/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button color="linear-gradient(top, #049cdb, #0064cd)" title="Usuarios"/>
+                    </View>
+                </View>
+            </View>
         </View>
-        );
-    }
+     );
+  }
 }
+
+const styles = StyleSheet.create({
+ container: {
+    paddingTop: 60,
+    paddingBottom: 60,
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+ },
+ title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 30,
+ },
+ content: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+ },
+ sections: {
+    marginTop: 60,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+ },
+ button: {
+    minWidth: 60,
+    marginRight: 10,
+    marginLeft: 10,
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    fontSize: 18,
+    lineHeight: 1.5,
+    color: "#fff",
+    textTransform: "uppercase",
+    width: "100%",
+    height: 50,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    backgroundColor: "#0064cd",
+    paddingTop: 0,
+    paddingRight: 25,
+    paddingBottom: 0,
+    paddingLeft: 25,
+    textShadowOffset: {
+      width: 0,
+      height: -1
+    },
+    textShadowRadius: 0,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    borderTopColor: "#0064cd",
+    borderRightColor: "#0064cd",
+    borderBottomColor: "#003f81",
+    borderLeftColor: "#0064cd"
+ },
+});
