b28a24a2a62d48ac80fb6f1c65eb76fb49cc96bc
marcadsan2 <marcadsan2@alum.us.esgit config --global user.email marcadsan2@alum.us.es>
2021-01-06 01:43:56 +0100

    VI-020: Envio resultado de una votacion a Twitter

--- decide/visualizer/twitterbot.py
+++ decide/visualizer/twitterbot.py
@@ -78,3 +78,34 @@ def send_twitter_report_voting(voting):
         auth.set_access_token(access_token, access_token_secret)
         api = tweepy.API(auth)
         return api.update_status(text)
+
+
+
+# Envío de reporte desde un objeto Votación Binaria
+def send_twitter_report_binary(v):
+    voting_id = v.id
+    voting_title = v.titulo
+    voting_desc = v.descripcion
+    voting_start = v.fecha_inicio
+    voting_end = v.fecha_fin
+    status = "Votación no comenzada"
+    if voting_start is not None:     
+        if voting_end is None:
+            a = str(voting_start).split(" ")[0].split("-")
+            start_date=a[2] + " del " + a[1] + " de " + a[0]
+            status = "Votación en Curso. Comenzada el " + start_date
+        else:
+            a = str(voting_end).split(" ")[0].split("-")
+            end_date=a[2] + " del " + a[1] + " de " + a[0]
+            status = "Votación Finalizada el " + end_date
+    text = "REPORTE DE ESTADO VOTACIÓN BINARIA\n\n"
+    text += "Votación: " + str(voting_id) + " - " + voting_title +"\n"
+    text += "Estado: " + status + "\n"
+    text += "Descripción: " + str(voting_desc) + "\n\n"
+    text += "Nº de respuestas Sí: " +str(VotacionBinaria.Numero_De_Trues(v)) + "\n"
+    text += "Nº de respuestas No: " +str(VotacionBinaria.Numero_De_Falses(v)) + "\n"
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api.update_status(text)
+
