b99513969c2ce897dcf177b66faaa2891a5f9659
fraromgon2us <fraromgon2@alum.us.es>
2021-01-10 11:26:55 +0100

    Codigo VO-044
    Implementacion de vistas de descarga de votaciones

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -9,12 +9,24 @@ urlpatterns = [
     path('votacionBinaria/<id>/', views.getVotacionBinariaById, name='Votacion Binaria'),
     path('votacionBinaria/all', views.getAllVotacionesBinarias, name='Votacion Binaria Todas'),
 
+    path('downloadBinaria/<id>', views.downloadVotacionBinaria, name='Download Binaria'),
+    path('downloadBinariaAll', views.downloadAllVotacionBinaria, name='Download Binaria All'),
+
     path('votacion/<id>/', views.getVotacionById, name='Votacion'),
     path('votacion/all', views.getAllVotaciones, name='Votacion Todas'),
+
+    path('downloadNormal/<id>', views.downloadVotacionNomral, name='Download Normal'),
+    path('downloadNormalAll', views.downloadAllVotacionNormal, name='Downlaod Normal All'),
 
     path('votacionMultiple/<id>/', views.getVotacionMultipleById, name='Votacion Multiple'),
     path('votacionMultiple/all', views.getAllVotacionesMultiples, name='Votacion Multiple Todas'),
 
+    path('downloadMultiple/<id>', views.downloadVotacionMultiple, name='Download Multiple'),
+    path('downloadMultipleAll', views.downloadAllVotacionMultiple, name='Download Multiple All'),
+
     path('votacionPreferencia/<id>/', views.getVotacionPreferenciaById, name='Votacion Preferencia'),
-    path('votacionPreferencia/all', views.getAllVotacionesPreferencia, name='Votacion Preferencia')
+    path('votacionPreferencia/all', views.getAllVotacionesPreferencia, name='Votacion Preferencia'),
+
+    path('downloadPreferencia/<id>', views.downloadVotacionPreferencia, name='Download Preferencia'),
+    path('downloadPreferenciaAll', views.downloadAllVotacionPreferencia, name='Download Preferencia All')
 ]
