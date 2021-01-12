ad9aced1fa243515553a0f09f42a35df8f1b158c
marcadsan2 <marcadsan2@alum.us.es>
2021-01-12 21:54:54 +0100

    VI-047: Resolver errores bo twitter

--- decide/visualizer/twitterbot.py
+++ decide/visualizer/twitterbot.py
@@ -5,6 +5,19 @@ consumer_secret_key = 'bOQoMVaIcYUrTzPba0ogudtIZi6HJZtfTdBaG09EVG60UN6k8Y'
 access_token = '1346151287306530817-DshaN1ufww133bG0S8KKSRFWpaBn0y'
 access_token_secret = 'vlIKntFymAL3Us2CcnkdbYpa1i2LJ9Q9RwVAt9OFSp668'
 
+def send_twitter_report(voting):
+    if isinstance(voting,Voting):
+        message = send_twitter_report_voting(voting)
+    elif isinstance(voting,VotacionBinaria):
+        message = send_twitter_report_binary(voting)
+    elif isinstance(voting,Votacion):
+        message = send_twitter_report_normal(voting)
+    elif isinstance(voting,VotacionPreferencia):
+        message = send_twitter_report_preferencia(voting)   
+    elif isinstance(voting,VotacionMultiple):
+        message = send_twitter_report_mult(voting)
+    return message  
+
 def send_twitter_report_json(voting):
     vot_id = voting.get('id')
     vot_name = voting.get('name')
@@ -21,20 +34,7 @@ def send_twitter_report_json(voting):
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     return api.update_status(tweet)
-
-def send_twitter_report(voting):
-    if isinstance(voting,Voting):
-        message = send_twitter_report_voting(voting)
-    elif isinstance(voting,VotacionBinaria):
-        message = send_twitter_report_binary(voting)
-    elif isinstance(voting,Votacion):
-        message = send_twitter_report_normal(voting)
-    elif isinstance(voting,VotacionPreferencia):
-        message = send_twitter_report_preferencia(voting)   
-    elif isinstance(voting,VotacionMultiple):
-        message = send_twitter_report_mult(voting)     
-    return message
-
+    
 def send_twitter_report_voting(voting):
     vot_id = voting.id
     vot_name = voting.name
@@ -49,7 +49,6 @@ def send_twitter_report_voting(voting):
     tweet += "\nResultados: \n"
     for option in post:
         tweet += option.get('option') + " - " + str(option.get('votes')) + " votos \n"
-
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
@@ -62,7 +61,6 @@ def send_twitter_report_binary(v):
    
     tweet = "Votación: " + v.titulo +"\n"
     tweet += "Descripción: " + v.descripcion +"\n"
-    tweet += vot_question + "\n"
     tweet += "\nResultados: \n"
     tweet += "Sí: " +str(VotacionBinaria.Numero_De_Trues(v)) + "\n"
     tweet += "No: " +str(VotacionBinaria.Numero_De_Falses(v)) + "\n"
@@ -70,7 +68,7 @@ def send_twitter_report_binary(v):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    return api.update_status(tweet)
+    return  api.update_status(tweet)
 
 def send_twitter_report_normal(v):
     voting_id = v.id
@@ -109,8 +107,7 @@ def send_twitter_report_preferencia(v):
         else:
             question_ans = question.opcionesRespuesta.all()
             for ans in question_ans:
-                tweet += "- " + ans.nombre_opcion + ": " + str(OpcionRespuesta.Media_Preferencia(ans))+"\n"
-    tweet += "\n"
+                tweet += ans.nombre_opcion + ": " + str(OpcionRespuesta.Media_Preferencia(ans))+"\n"
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
