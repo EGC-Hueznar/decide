0a05ccc4ecf92d856b0f42d773367ed87fbe82b9
dml1994 <dml94@live.com>
2021-01-12 21:21:18 +0100

    VI046 - Adaptado el codigo para las nuevas votaciones
    Se ha cambiado el metodo en views.py para trabajar directamente con los objetos de votacion en lugar de un json y metodos seperados

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<str:tipo>/', VisualizerVotingList.as_view()),
     path('<str:tipo>/<int:voting_id>/', VisualizerVista.as_view()),
     path('<int:voting_id>/telegram_report', telegram_report),
+    path('<str:tipo>/<int:voting_id>/telegram_report', telegram_report),
 ]
