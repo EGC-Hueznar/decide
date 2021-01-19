0fbb0e8faa8ce72aa41206c24f45c79d4a856963
marcadsan2 <marcadsan2@alum.us.es>
2021-01-19 18:29:43 +0100

    VI-056: Cambio del mensaje del bot de telegram

--- decide/visualizer/telegrambot.py
+++ decide/visualizer/telegrambot.py
@@ -24,35 +24,35 @@ def send_telegram_report_voting(voting):
         voting_end = voting.end_date
         voting_tally = voting.tally
 
-        status = "Votación no comenzada"
+        status = "Not started voting"
         if voting_start is not None:     
             if voting_end is None:
                 a = str(voting_start).split(" ")[0].split("-")
-                start_date=a[2] + " del " + a[1] + " de " + a[0]
-                status = "Votación en Curso. Comenzada el " + start_date
+                start_date=a[0] + " - " + a[1] + " - " + a[2]
+                status = "Started voting. Date: " + start_date
             else:
                 a = str(voting_end).split(" ")[0].split("-")
-                end_date=a[2] + " del " + a[1] + " de " + a[0]
-                status = "Votación Finalizada el " + end_date
+                end_date=a[0] + " del " + a[1] + " de " + a[2]
+                status = "Finished voting. Date: " + end_date
 
         options = voting_q.options.all()
         post = voting.postproc
 
-        text = "REPORTE DE ESTADO VOTING\n\n"
-        text += "Votación: " + str(voting_id) + " - " + voting_name +"\n"
-        text += "Estado: " + status + "\n"
-        text += "Descripción: " + str(voting_desc) + "\n\n"
+        text = "Not started voting\n\n"
+        text += "Voting: " + str(voting_id) + " - " + voting_name +"\n"
+        text += "Status: " + status + "\n"
+        text += "Description: " + str(voting_desc) + "\n\n"
         if voting_tally is None:
-            text += "Pregunta: " + str(voting_q) + "\n"
+            text += "Question: " + str(voting_q) + "\n"
             for option in options:
-                text += "Opción " + str(option.number) + " - " + option.option+ "\n"
+                text += "Option: " + str(option.number) + " - " + option.option+ "\n"
             if voting_end is not None:
-                text += "\nEsperando el resultado del recuento"
+                text += "\nWaiting tally"
         else:
-            text += "Pregunta: " + str(voting_q) + "\n"
+            text += "Question: " + str(voting_q) + "\n"
             for option in post:
-                text += "Opción " + str(option.get('number')) + " - " + option.get('option') + " - " + str(option.get('votes')) + " votos \n"
-            text += "\nResultados de la Votación: \n"
+                text += "Option " + str(option.get('number')) + " - " + option.get('option') + " - " + str(option.get('votes')) + " votes \n"
+            text += "\nResults: \n"
             for option in post:
                 text += option.get('option') + " - " + str(option.get('postproc')) + "\n"
         
@@ -68,22 +68,22 @@ def send_telegram_report_binary(v):
     voting_desc = v.descripcion
     voting_start = v.fecha_inicio
     voting_end = v.fecha_fin
-    status = "Votación no comenzada"
+    status = "Not started voting"
     if voting_start is not None:     
         if voting_end is None:
             a = str(voting_start).split(" ")[0].split("-")
-            start_date=a[2] + " del " + a[1] + " de " + a[0]
-            status = "Votación en Curso. Comenzada el " + start_date
+            start_date=a[0] + " - " + a[1] + " - " + a[2]
+            status = "Started voting. Date: " + start_date
         else:
             a = str(voting_end).split(" ")[0].split("-")
-            end_date=a[2] + " del " + a[1] + " de " + a[0]
-            status = "Votación Finalizada el " + end_date
-    text = "REPORTE DE ESTADO VOTACIÓN BINARIA\n\n"
-    text += "Votación: " + str(voting_id) + " - " + voting_title +"\n"
-    text += "Estado: " + status + "\n"
-    text += "Descripción: " + str(voting_desc) + "\n\n"
-    text += "Nº de respuestas Sí: " +str(VotacionBinaria.Numero_De_Trues(v)) + "\n"
-    text += "Nº de respuestas No: " +str(VotacionBinaria.Numero_De_Falses(v)) + "\n"
+            end_date=a[2] + " - " + a[1] + " - " + a[0]
+            status = "Finished voting. Date:  " + end_date
+    text = "Preference voting Report\n\n"
+    text += "Voting: " + str(voting_id) + " - " + voting_title +"\n"
+    text += "Status: " + status + "\n"
+    text += "Description: " + str(voting_desc) + "\n\n"
+    text += "Yes: " +str(VotacionBinaria.Numero_De_Trues(v)) + "\n"
+    text += "No: " +str(VotacionBinaria.Numero_De_Falses(v)) + "\n"
     bot_token = '1415070510:AAE49OJPu4viYNo5Tfov4vzkoIyeNf_JBr4'
     bot = telegram.Bot(bot_token)
     id = -1001318632551
@@ -170,24 +170,24 @@ def send_telegram_report_mult(v):
     voting_start = v.fecha_inicio
     voting_end = v.fecha_fin
     voting_questions = v.preguntasMultiples.all()
-    status = "Votación no comenzada"
+    status = "Not started voting"
     if voting_start is not None:     
         if voting_end is None:
             a = str(voting_start).split(" ")[0].split("-")
-            start_date=a[2] + " del " + a[1] + " de " + a[0]
-            status = "Votación en Curso. Comenzada el " + start_date
+            start_date=a[0] + " - " + a[1] + " - " + a[2]
+            status = "Started voting. Date: " + start_date
         else:
             a = str(voting_end).split(" ")[0].split("-")
-            end_date=a[2] + " del " + a[1] + " de " + a[0]
-            status = "Votación Finalizada el " + end_date
-    text = "REPORTE DE ESTADO VOTACIÓN MÚLTIPLE\n\n"
-    text += "Votación: " + str(voting_id) + " - " + voting_title +"\n"
-    text += "Estado: " + status + "\n"
-    text += "Descripción: " + str(voting_desc) + "\n\n"
+            end_date=a[0] + " - " + a[1] + " - " + a[2]
+            status = "Finish voting. Date: " + end_date
+    text = "Multiple voting Report\n\n"
+    text += "Voting: " + str(voting_id) + " - " + voting_title +"\n"
+    text += "Status: " + status + "\n"
+    text += "Description: " + str(voting_desc) + "\n\n"
     for question in voting_questions:
         text += question.textoPregunta + "\n"
         if PreguntaMultiple.Numero_De_Opciones(question) == 0 :
-            text += "No hay opciones para esta pregunta \n"
+            text += "There are not any options. \n"
         else:
             question_ans = question.opcionesMultiples.all()
             for ans in question_ans:
